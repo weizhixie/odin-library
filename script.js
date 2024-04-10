2773643a87a8cb99bba884be0e1b7fa5b8696a58
weizhixie <wx02697@yahoo.co.uk>
2024-04-10 16:18:16 +0100

    Add display and create book card function and update ui

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-const myLibrary = [];
+const myLibrary = [
+    { title: "Harry Potter", author: "J. K. Rowling", pages: 607, status: "Read" },
+    { title: "The Hobbit", author: "J. R. R. Tolkien", pages: 372, status: "Not Read" },
+    { title: "Dream of the Red Chamber", author: "Cao Xueqin", pages: 352, status: "Read" },
+];
+const bookContainer = document.querySelector(".book-container");
 
-function Book(title, author, pages) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.status = status;
 }
 
-function addBookToLibrary(title, author, pages) {
-    const newBook = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, status) {
+    const newBook = new Book(title, author, pages, status);
     myLibrary.push(newBook);
 }
+
+function createBookCard(title, author, pages, status) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    const bookTitle = document.createElement("h2");
+    bookTitle.textContent = title;
+    bookCard.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = `By: ${author}`;
+    bookCard.appendChild(bookAuthor);
+
+    const bookPages = document.createElement("p");
+    bookPages.textContent = `Pages: ${pages}`;
+    bookCard.appendChild(bookPages);
+
+    const bookStatus = document.createElement("p");
+    bookStatus.textContent = `Status: ${status}`;
+    bookCard.appendChild(bookStatus);
+
+    return bookCard;
+}
+
+function displayBookCard() {
+    for (const book of myLibrary) {
+        bookContainer.appendChild(createBookCard(book["title"], book["author"], book["pages"], book["status"]));
+    }
+}
+
+displayBookCard();
